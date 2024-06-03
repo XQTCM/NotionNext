a4b210794864ba2a2cd4c0beef17c14906a43dd6
XQTCM <chaney.wang@hotmail.com>
2024-06-03 22:13:12 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -24,7 +24,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'DrWangXQ', // 您的昵称 例如 xqtcm
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '王宣权大夫', // 您的昵称 例如 xqtcm
   BIO: process.env.NEXT_PUBLIC_BIO || '一个经络迷🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://jingluomi.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Jingluomi, 经络迷', // 网站关键词 英文逗号隔开
