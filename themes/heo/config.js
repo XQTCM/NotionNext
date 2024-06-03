fe877e8e5db9b2506ec60959d88ca8e04572511d
XQTCM <chaney.wang@hotmail.com>
2024-06-03 22:06:20 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,7 +5,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+    { title: '欢迎来到我的博客', url: 'https://www.jingluomi.com' },
     { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
   ],
 
@@ -17,10 +17,12 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享中医',
   HEO_HERO_TITLE_2: '与临床思维',
-  HEO_HERO_TITLE_3: 'XQTCM.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_3: '探索经络之迷Jing
+LuoMI.COM',
+  HEO_HERO_TITLE_4: '古典与创新的融合',
+  HEO_HERO_TITLE_5: 'jingluomi
+经络迷-发现经络之美',
+  HEO_HERO_TITLE_LINK: 'https://jingluomi.com',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
