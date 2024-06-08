85c4323d6edbfb8df46b865800fc083383d1fb1e
XQTCM <chaney.wang@hotmail.com>
2024-06-08 21:29:33 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -27,7 +27,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '经络迷王大夫', // 您的昵称 例如 xqtcm
   BIO: process.env.NEXT_PUBLIC_BIO || '是一个经络迷🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://jingluomi.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Jingluomi, 经络迷', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Jingluomi, 王大夫是经络迷', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
