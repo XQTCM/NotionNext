43fa39088048820ff23a33f6e11a3ba81f294330
XQTCM <chaney.wang@hotmail.com>
2024-06-11 21:06:29 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://www.jingluomi.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '访问文档中心获取更多帮助', url: 'https://docs.jingluomi.com' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -19,13 +19,13 @@ const CONFIG = {
   HEO_HERO_TITLE_2: '与临床思维',
   HEO_HERO_TITLE_3: '探索经络之迷JingLuoMI.COM',
   HEO_HERO_TITLE_4: '古典与创新的融合',
-  HEO_HERO_TITLE_5: 'jingluomi经络迷-发现经络之美',
+  HEO_HERO_TITLE_5: '经络迷&发现经络之美',
   HEO_HERO_TITLE_LINK: 'https://jingluomi.com',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
   HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用中医', url: '/tag/实用中医' },
+  HEO_HERO_CATEGORY_3: { title: '实用穴位', url: '/tag/实用穴位' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -40,7 +40,7 @@ const CONFIG = {
     '🏃 专注临床实践',
     '🏠 健康生活守护者',
     '🤖️ 中医创新探索者',
-    '🧱 疾病调理专家'
+    '🧱 经络调理专家'
   ],
   HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
 
@@ -108,7 +108,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://docs.jingluomi.com/article/how-to-question',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
